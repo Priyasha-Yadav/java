939408658d38ee18f3f8c55232a748596cb824be
Priyasha-Yadav <priyasha.yadav.cg@gmail.com>
2025-08-13 07:22:45 +0530

    Added input.java and inter=gers.java

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args){
+        String str;
+        int num;
+        float fnum;
+        Scanner sc = new Scanner(System.in);
+        
+
+        
+    } 
+}
